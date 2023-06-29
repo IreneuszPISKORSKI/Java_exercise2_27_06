b1b719a0a81f98be85dead1c2b9c24262a4a1e15
Ireneusz.Piskorski <ireneusz.piskorski@le-campus-numerique.fr>
2023-06-29 16:59:24 +0200

    Ex4 CRUD and show - ok

--- src/main/java/com/iter2/java_advanced_thymleaf/controller/MainController.java
+++ src/main/java/com/iter2/java_advanced_thymleaf/controller/MainController.java
@@ -6,24 +6,25 @@ import java.util.Optional;
 import com.iter2.java_advanced_thymleaf.form.CharacterForm;
 import com.iter2.java_advanced_thymleaf.character.Character;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+
 @Controller
 public class MainController {
 
     private static final List<Character> characterList = new ArrayList<Character>();
-    private int lastId=2;
+    private int lastId=0;
 
-    static {
-        characterList.add(new Character(1,"name1", "Mage", 8));
-        characterList.add(new Character(2,"name2", "Warrior", 10));
-    }
 
-    // Injectez (inject) via application.properties.
     @Value("${welcome.message}")
     private String message;
 
@@ -38,20 +39,24 @@ public class MainController {
 
     @RequestMapping(value = {"/listCharacter"}, method = RequestMethod.GET)
     public String characterList(Model model){
-        model.addAttribute("characterList", characterList);
+        RestTemplate restTemplate = new RestTemplate();
+        Character[] characters = restTemplate.getForObject("http://localhost:8081/api/character", Character[].class);
+        model.addAttribute("characterList", characters);
         return "listCharacter";
     }
 
     @RequestMapping(value = {"/edit/{id}"}, method = RequestMethod.GET)
     public String ditCharacterById(Model model, @PathVariable Integer id){
-        Optional<Character> selectedCharacter = characterList.stream().filter(ch->ch.getId()==id).findFirst();
-        if (selectedCharacter.isPresent()){
-            model.addAttribute("characterForm", selectedCharacter.get());
+        RestTemplate restTemplate = new RestTemplate();
+        Character selectedCharacter = restTemplate.getForObject("http://localhost:8081/api/character/" + id, Character.class);
+        if (selectedCharacter != null){
+            model.addAttribute("characterForm", selectedCharacter);
         }else {
             return "redirect:/listCharacter";
         }
         return "characterDetails";
     }
+
     @RequestMapping(value = {"/edit/{id}"}, method = RequestMethod.POST)
     public String saveEditCharacter(Model model,//
                                 @ModelAttribute("characterForm") CharacterForm characterForm,
@@ -59,17 +64,22 @@ public class MainController {
         String name = characterForm.getName();
         String type = characterForm.getType();
         int hp = characterForm.getHp();
+        RestTemplate restTemplate = new RestTemplate();
 
         if ( id > 0 && hp > 0 //
                 && name != null && name.length() > 0//
                 && type != null && type.length() > 0) {
-            Optional<Character> selectedCharacter = characterList.stream().filter(ch->ch.getId()==id).findFirst();
-            if (selectedCharacter.isPresent()){
-                selectedCharacter.get().setName(characterForm.getName());
-                selectedCharacter.get().setType(characterForm.getType());
-                selectedCharacter.get().setHp(characterForm.getHp());
-            }
+            Character selectedCharacter = restTemplate.getForObject("http://localhost:8081/api/character/" + id, Character.class);
+            if (selectedCharacter != null){
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                selectedCharacter.setName(name);
+                selectedCharacter.setType(type);
+                selectedCharacter.setHp(hp);
 
+                HttpEntity<Character> request = new HttpEntity<>(selectedCharacter, headers);
+                restTemplate.exchange("http://localhost:8081/api/character/" + id, HttpMethod.PUT, request , Character.class);
+            }
             return "redirect:/listCharacter";
         }
 
@@ -86,6 +96,8 @@ public class MainController {
     @RequestMapping(value = {"/addCharacter"}, method = RequestMethod.POST)
     public String saveCharacter(Model model,//
             @ModelAttribute("characterForm") CharacterForm characterForm){
+
+
         String name = characterForm.getName();
         String type = characterForm.getType();
         int hp = characterForm.getHp();
@@ -95,21 +107,27 @@ public class MainController {
                 && type != null && type.length() > 0) {
             lastId++;
             int id = lastId;
-            Character newCharacter = new Character(id, name, type, hp);
-            characterList.add(newCharacter);
 
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            RestTemplate restTemplate = new RestTemplate();
+
+            Character newCharacter = new Character(id, name, type, hp);
+
+            HttpEntity<Character> request = new HttpEntity<>(newCharacter, headers);
+            restTemplate.postForEntity("http://localhost:8081/api/character", request , Character.class);
             return "redirect:/listCharacter";
         }
 
         model.addAttribute("errorMessage", errorMessage);
-        return "addCharacter";    }
+        return "addCharacter";
+    }
 
     @RequestMapping(value = {"/delete/{id}"}, method = RequestMethod.GET)
     public String deleteCharacter(@PathVariable int id){
-        Character selectedCharacter = characterList.stream().filter(ch->ch.getId()==id).findFirst().get();
-
-        characterList.remove(selectedCharacter);
-
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.delete("http://localhost:8081/api/character/" + id);
         return "redirect:/listCharacter";
     }
 }
